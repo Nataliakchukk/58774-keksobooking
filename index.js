2e906a743a20f9c5d7aab2a4b326420484a07c65
Nadya <nadin.ts@gmail.com>
2018-02-08 12:28:24 +0300

    Убрала модули, теперь используется process.argv

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Enter commands name [--version, --help] ', (answer) => {
-  switch (answer) {
-    case '--version':
-      console.log('version 1.2.3');
-      break;
-    case '--help':
-      console.log('help: print info.');
-      break;
-    case '':
-      console.log('your answer is empty');
-      break;
-    default:
-      console.log(`error: your answer ${answer} is not good, try again`)
-
-  }
-  rl.close();
-});
+const args = process.argv.slice(2);
+const arreyValueProcess = process.argv[1].split(`/`);
+if (process.argv.length > 2) {
+   switch (args[0]) {
+      case `--help`:
+        console.log(`печатает этот текст`);
+        break;
+      case `--version`:
+        console.log(`version 1.2.3`);
+        break;
+      default:
+        console.log(`Неизвестная команда ${args[0]} Чтобы прочитать правила использования приложения, наберите "--help"`);
+    };
+} else {
+    console.log(`Привет ${arreyValueProcess[2]}! Эта программа будет запускать сервер «${arreyValueProcess[3]}».`);
+    process.exit(1);
+}

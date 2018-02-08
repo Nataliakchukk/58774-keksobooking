9d1aaff0070d63c3ff736e14571ac207e256d9ba
Nadya <nadin.ts@gmail.com>
2018-02-08 15:19:58 +0300

    удалила избыточный код

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const args = process.argv.slice(2);
-const arreyValueProcess = process.argv[1].split(`/`);
 if (process.argv.length > 2) {
    switch (args[0]) {
       case `--help`:
@@ -10,8 +9,8 @@ if (process.argv.length > 2) {
         break;
       default:
         console.log(`Неизвестная команда ${args[0]} Чтобы прочитать правила использования приложения, наберите "--help"`);
+        process.exit(1);
     };
 } else {
-    console.log(`Привет ${arreyValueProcess[2]}! Эта программа будет запускать сервер «${arreyValueProcess[3]}».`);
-    process.exit(1);
+    console.log(`Привет Nadya! Эта программа будет запускать сервер «Кексобукинг».`);
 }

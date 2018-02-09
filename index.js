5f05cbf6becdcab734ec2ef2e4a17c35231c99ee
Nadya <nadin.ts@gmail.com>
2018-02-09 11:24:42 +0300

    добавила package.json и eslint

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const args = process.argv.slice(2);
 if (args.length > 0) {
-   switch (args[0]) {
-      case `--help`:
-        console.log(`Доступные команды:
-          --help    — печатает этот текст;
-          --version — печатает версию приложения;`);
-        break;
-      case `--version`:
-        console.log(`version 1.2.3`);
-        break;
-      default:
-        console.log(`Неизвестная команда ${args[0]} Чтобы прочитать правила использования приложения, наберите "--help"`);
-        process.exit(1);
-    };
+  switch (args[0]) {
+    case `--help`:
+      console.log(`Доступные команды:
+        --help    — печатает этот текст;
+        --version — печатает версию приложения;`);
+      break;
+    case `--version`:
+      console.log(`version 1.2.3`);
+      break;
+    default:
+      console.log(`Неизвестная команда ${args[0]} Чтобы прочитать правила использования приложения, наберите "--help"`);
+      process.exit(1);
+  }
 } else {
-    console.log(`Привет Nadya! Эта программа будет запускать сервер «Кексобукинг».`);
+  console.log(`Привет Nadya! Эта программа будет запускать сервер «Кексобукинг».`);
 }

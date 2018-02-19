c9b88d15f54d02901eb683da81b33025d0669432
Nadya <nadin.ts@gmail.com>
2018-02-19 17:20:18 +0300

    yes/no заработало

--- src/default-command.js
+++ src/default-command.js
@@ -1,3 +1,4 @@
+const fs = require(`fs`);
 const colors = require(`colors/safe`);
 const packageInfo = require(`../package.json`);
 const generateEntity = require(`./generateEntity`);
@@ -12,10 +13,26 @@ module.exports = {
       output: process.stdout
     });
     console.log(`Привет ${colors.rainbow(packageInfo.author)}! Эта программа будет запускать сервер «${colors.rainbow(packageInfo.name)}».`);
-    rl.question('Сколько объектов недвижимости нужно создать? ', (count) => {
-      rl.question('Укажите путь, куда сохранять данные? ', (path) => {
-        generateEntity.execute(count, path);
-        rl.close();
+    rl.question(`Сколько объектов недвижимости нужно создать? `, (count) => {
+      rl.question(`Укажите путь, куда сохранять данные? `, (path) => {
+        const filePath = path && path.indexOf(`.json`) !== -1 ? path : `${process.cwd()}/wizards-data.json`;
+        fs.readFile(filePath, (err, data) => {
+          if (err) {
+            rl.question(`Файл ${filePath} уже существует, перезаписать?`, (answer) => {
+              if (answer === `yes`) {
+                console.log(`файл перезаписался`);
+                generateEntity.execute(count, filePath);
+              } else {
+                console.log(`файл остался прежним`);
+              }
+              rl.close();
+            });
+          } else {
+            generateEntity.execute(count, filePath);
+            rl.close();
+          }
+          rl.close();
+        });
       });
     });
   }
--- src/generateEntity.js
+++ src/generateEntity.js
@@ -6,40 +6,18 @@ const readline = require('readline');
 
 const fileWriteOptions = {encoding: `utf-8`, mode: 0o644};
 
+
 module.exports = {
   name: `generateEntity`,
   description: `Generates data for project`,
-  execute(count, path) {
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    const filePath = path && path.indexOf(`.json`) !== -1 ? path : `${process.cwd()}/wizards-data.json`;
-    const countData = count && typeof +count === `number`? count : 1;
+  execute(filePath = `${process.cwd()}/wizards-data.json`) {
     const setData = (number, path) => {
       let dataArrey = [];
       while (number > 0) {
          dataArrey.push(generateEntity());
          number--;
       };
-      return dataArrey;
       writeFile(path, JSON.stringify(dataArrey), fileWriteOptions);
     };
-    const refreshData = (count) => {
-      if (count === `yes`) {
-        console.log(`файл перезаписался`);
-        setData(countData, filePath);
-      } else {
-        console.log(`файл остался прежним`);
-      }
-    };
-
-    fs.readFile(filePath, (err, data) => {
-      if (err) {
-        rl.question(`Файл ${filePath} уже существует, перезаписать?`, (answer) => {
-          refreshData(answer);
-        });
-      }
-    });
   }
 };

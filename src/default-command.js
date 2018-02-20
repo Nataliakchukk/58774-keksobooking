fba094c4e15a81ea816264900f725beebcd10626
Nadya <nadin.ts@gmail.com>
2018-02-20 09:39:30 +0300

    fix & refactoring.

--- src/default-command.js
+++ src/default-command.js
@@ -1,10 +1,9 @@
 const fs = require(`fs`);
-const colors = require(`colors/safe`);
 const packageInfo = require(`../package.json`);
 const {generateEntity} = require(`../src/generator/wizards-generator`);
 const util = require(`util`);
 const writeFile = util.promisify(fs.writeFile);
-const readline = require('readline');
+const readline = require(`readline`);
 const open = util.promisify(fs.open);
 
 
@@ -15,49 +14,52 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const question = util.promisify(function(quest, callback) {
+const question = util.promisify(function (quest, callback) {
   rl.question(quest, callback.bind(null, null));
 });
 
-const setData = (count, filePath) => {
+const setData = util.promisify(function (count, filePath, callback) {
   let dataArrey = [];
   while (count > 0) {
     dataArrey.push(generateEntity());
     count--;
-  };
+  }
   writeFile(filePath, JSON.stringify(dataArrey), fileWriteOptions)
-};
+      .then(() => {
+        callback();
+      });
+});
 
 const setFilePath = (filePath, count) => {
-  return open(filePath, `wx`)
-    .then(() => {
-      return setData(count, filePath)
-        .then(() => {
-          console.log(`Файл записан`);
-          rl.close();
-          process.exit(0);
-      });
-    })
-    .catch((pathError) => {
-      if(pathError) {
-        if(pathError.code === `EEXIST`) {
-          return question(`Файл уже есть, перезаписать? [yes/no]: `)
-            then((rewriteFile) => {
-              if (rewriteFile !== `yes`) {
-                console.log(`Файл остался прежним`);
-                rl.close();
-                process.exit(0);
-              }
-            return setData(count, filePath)
-              .then(() => {
-                console.log(`Файл перезаписан`);
-                rl.close();
-                process.exit(0);
-              });
-          });
+  open(filePath, `wx`)
+      .then(() => {
+        setData(count, filePath)
+            .then(() => {
+              console.log(`Файл записан`);
+              rl.close();
+              process.exit(0);
+            });
+      })
+      .catch((pathError) => {
+        if (pathError) {
+          if (pathError.code === `EEXIST`) {
+            question(`Файл уже есть, перезаписать? [yes/no]: `)
+                .then((rewriteFile) => {
+                  if (rewriteFile !== `yes`) {
+                    console.log(`Файл остался прежним`);
+                    rl.close();
+                    process.exit(0);
+                  }
+                  setData(count, filePath)
+                      .then(() => {
+                        console.log(`Файл перезаписан`);
+                        rl.close();
+                        process.exit(0);
+                      });
+                });
+          }
         }
-      }
-    });
+      });
 };
 
 module.exports = {
@@ -66,11 +68,11 @@ module.exports = {
   execute() {
     console.log(`Привет ${packageInfo.author}! Эта программа будет запускать сервер «${packageInfo.name}».`);
     question(`Сколько объектов сформировать? `)
-      .then((count) => {
-        return question(`Укажите путь, где сохранить данные: `)
-          .then((filePath) => {
-            setFilePath(filePath, count);
+        .then((count) => {
+          question(`Укажите путь, где сохранить данные: `)
+              .then((filePath) => {
+                setFilePath(filePath, count);
+              });
         });
-      });
   }
 };
--- test/generator.test.js
+++ test/generator.test.js
@@ -1,28 +1,6 @@
-const generateCommand = require(`../src/generateEntity`);
 const {generateEntity} = require(`../src/generator/wizards-generator`);
 const data = require(`../src/data`);
-const fs = require(`fs`);
-const {promisify} = require(`util`);
-const access = promisify(fs.access);
-const unlink = promisify(fs.unlink);
 const assert = require(`assert`);
-
-
-describe(`Generate JSON command`, function () {
-  it(`should fail on not existing folder`, function () {
-    const tempFileName = `${__dirname}/folder/testfile.json`;
-    return generateCommand.execute(tempFileName)
-        .then(() => assert.fail(`Path ${tempFileName} should not be available`))
-        .catch((e) => assert.ok(e));
-  });
-
-  it(`should create new file`, function () {
-    const templFileName = `${__dirname}/testfile.json`;
-    return generateCommand.execute(templFileName)
-        .then(() => access(templFileName))
-        .then(() => unlink(templFileName));
-  });
-});
 
 describe(`Generate arrey`, () => {
   const generator = generateEntity();

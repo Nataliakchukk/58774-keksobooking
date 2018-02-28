69b6aaab44cdfe79863707ade6fb54339fafe783
Nadya <nadin.ts@gmail.com>
2018-02-28 21:44:13 +0300

    добавила тесты

--- test/post-offer.js
+++ test/post-offer.js
@@ -85,4 +85,104 @@ describe(`POST /api/offers`, function () {
           type: `flat`,
         });
   });
+
+  it(`should fail if title is invalid`, () => {
+    return request(app).post(`/api/offers`).
+        field(`title`, `Г`).
+        field(`address`, `565.0488536141017, 335.1362499791128`).
+        field(`checkin`, `12:00`).
+        field(`checkout`, `14:00`).
+        field(`description`, ``).
+        field(`features`, [`wifi`, `dishwasher`, `parkin`, `washer`, `elevator`, `conditioner`]).
+        field(`guests`, `3`).
+        field(`price`, 545130).
+        field(`rooms`, 3).
+        field(`type`, `flat`).
+        attach(`avatar`, `test/fixtures/test.jpg`).
+        expect(400, [{
+          fieldName: `username`,
+          fieldValue: `Г`,
+          errorMessage: `should be in range 30..140`
+        }]);
+  });
+
+  it(`should fail if address is invalid`, () => {
+    return request(app).post(`/api/offers`).
+        field(`title`, `Маленький ужасный дворец`).
+        field(`address`, ``).
+        field(`checkin`, `12:00`).
+        field(`checkout`, `14:00`).
+        field(`description`, ``).
+        field(`features`, [`wifi`, `dishwasher`, `parkin`, `washer`, `elevator`, `conditioner`]).
+        field(`guests`, `3`).
+        field(`price`, 545130).
+        field(`rooms`, 3).
+        field(`type`, `flat`).
+        attach(`avatar`, `test/fixtures/test.jpg`).
+        expect(400, [{
+          fieldName: `address`,
+          fieldValue: ``,
+          errorMessage: `should be in range 1..100`
+        }]);
+  });
+  it(`should fail if checkin is invalid`, () => {
+    return request(app).post(`/api/offers`).
+        field(`title`, `Маленький ужасный дворец`).
+        field(`address`, `99`).
+        field(`checkin`, `0`).
+        field(`checkout`, `14:00`).
+        field(`description`, ``).
+        field(`features`, [`wifi`, `dishwasher`, `parkin`, `washer`, `elevator`, `conditioner`]).
+        field(`guests`, `3`).
+        field(`price`, 545130).
+        field(`rooms`, 3).
+        field(`type`, `flat`).
+        attach(`avatar`, `test/fixtures/test.jpg`).
+        expect(400, [{
+          fieldName: `checkin`,
+          fieldValue: `0`,
+          errorMessage: `should be formatt HH:mm`
+        }]);
+  });
+
+  it(`should fail if checkout is invalid`, () => {
+    return request(app).post(`/api/offers`).
+        field(`title`, `Маленький ужасный дворец`).
+        field(`address`, `99`).
+        field(`checkin`, `14:00`).
+        field(`checkout`, `0`).
+        field(`description`, ``).
+        field(`features`, [`wifi`, `dishwasher`, `parkin`, `washer`, `elevator`, `conditioner`]).
+        field(`guests`, `3`).
+        field(`price`, 545130).
+        field(`rooms`, 3).
+        field(`type`, `flat`).
+        attach(`avatar`, `test/fixtures/test.jpg`).
+        expect(400, [{
+          fieldName: `checkout`,
+          fieldValue: `0`,
+          errorMessage: `should be formatt HH:mm`
+        }]);
+  });
+
+  it(`should fail if rooms is invalid`, () => {
+    return request(app).post(`/api/offers`).
+        field(`title`, `Маленький ужасный дворец`).
+        field(`address`, `99`).
+        field(`checkin`, `14:00`).
+        field(`checkout`, `14:00`).
+        field(`description`, ``).
+        field(`features`, [`wifi`, `dishwasher`, `parkin`, `washer`, `elevator`, `conditioner`]).
+        field(`guests`, `3`).
+        field(`price`, 545130).
+        field(`rooms`, -1).
+        field(`type`, `flat`).
+        attach(`avatar`, `test/fixtures/test.jpg`).
+        expect(400, [{
+          fieldName: `rooms`,
+          fieldValue: `-1`,
+          errorMessage: `should be in range 1..1000`
+        }]);
+  });
+
 });
